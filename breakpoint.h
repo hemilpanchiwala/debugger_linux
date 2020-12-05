a2c02557cd5d1381404a6db4866921f1d682c856
Hemil Panchiwala <hpanchiwala@cs.iitr.ac.in>
2020-12-05 17:25:13 +0530

    add breakpoint class with enable and disable methods

---
+++ breakpoint.h
@@ -0,0 +1,59 @@
+#include <sys/wait.h>
+#include <sys/ptrace.h>
+#include <unistd.h>
+#include <bits/stdc++.h>
+
+class breakpoint {
+
+    public:
+        breakpoint(pid_t pid, intptr_t addr) : m_pid{pid}, m_addr{addr}, m_enabled{false}, m_data{} {}
+
+        void enable();
+        void disable();
+
+        auto is_enabled() const->bool {
+            return m_enabled;
+        }
+        auto get_addr() const->intptr_t {
+            return m_addr;
+        }
+
+    private:
+        pid_t m_pid;
+        intptr_t m_addr;
+        uint8_t m_data;
+        bool m_enabled;
+
+};
+
+// Adding 0xcc to the data of particular process and address defines the unique behaviour of breakpoint
+void breakpoint::enable() {
+
+    // Get data using ptrace using process ID and address
+    auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr);
+
+    // Saving the bottom bytes of data
+    m_bottom_data = static_cast<uint8_t>(data & 0xff);
+    
+    // Set bottom bytes to 0xcc
+    uint64_t updated_data = ((data & ~0xff) | 0xcc);
+
+    // Updating data at the address
+    ptrace(PTRACE_POKEDATA, m_pid, m_addr, updated_data);
+    m_enabled = true;
+
+}
+
+void breakpoint::disable() {
+
+    // Get data using ptrace using process ID and address
+    auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr);
+
+    // Restoring back the data by attaching the removed bottom bytes
+    restored_data = ((data & ~0xff) | m_bottom_data);
+
+    // Updating data at the address
+    ptrace(PTRACE_POKEDATA, m_pid, m_addr, restored_data);
+    m_enabled = false;
+
+}
