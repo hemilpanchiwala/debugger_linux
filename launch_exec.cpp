089a0b33b3f3f47aca478267062be7fb5de28721
Hemil Panchiwala <hpanchiwala@cs.iitr.ac.in>
2020-12-11 12:42:13 +0530

    add source-level stepping to debugger

--- launch_exec.cpp
+++ launch_exec.cpp
@@ -48,6 +48,16 @@ class debugger {
         siginfo_t get_signal_info();
         void wait_for_signal();
         void handle_bptrap(siginfo_t);
+        void single_step_instruction();
+        void single_step_instruction_with_bp_check();
+        uint64_t read_memory(uint64_t addr);
+        void write_memory(uint64_t addr, uint64_t value);
+        void remove_breakpoint(intptr_t addr);
+        uint64_t get_offset_program_counter();
+        uint64_t get_offset_dwarf_address(uint64_t addr);
+        void step_out();
+        void step_in();
+        void step_over();
 
     private:
         string m_prog_name;
@@ -105,16 +115,36 @@ void debugger::runCommand(const string& line) {
         string addr {args[2], 2};
 
         if(is_prefix(args[1], "read")) {
-            cout<<"READ: "<<ptrace(PTRACE_PEEKDATA, m_pid, stol(addr, 0, 16), nullptr)<<endl;
+            cout<<"READ: "<<read_memory(stol(addr, 0, 16))<<endl;
         } else if(is_prefix(args[1], "write")) {
             string value {args[3], 2};
-            ptrace(PTRACE_POKEDATA, m_pid, stol(addr, 0, 16), stol(value, 0, 16));
+            write_memory(stol(addr, 0, 16), stol(value, 0, 16));
         }
 
+    } else if (is_prefix(input_command, "stepinst")) {
+        
+        single_step_instruction_with_bp_check();
+        auto line_entry = get_line_entry_using_pc(get_program_counter());
+        print_source(line_entry->file->path, line_entry->line, 2);
+
+    } else if (is_prefix(input_command, "step")) {
+        step_in();
+    } else if (is_prefix(input_command, "next")) {
+        step_over();
+    } else if (is_prefix(input_command, "finish")) {
+        step_out();
     } else {
         cerr<<"No command found!! \n";
     }
 
+}
+
+uint64_t debugger::read_memory(uint64_t addr) {
+    return ptrace(PTRACE_PEEKDATA, m_pid, addr, nullptr);
+}
+
+void debugger::write_memory(uint64_t addr, uint64_t value) {
+    ptrace(PTRACE_POKEDATA, m_pid, addr, value);
 }
 
 void debugger::continue_execution() {
@@ -303,6 +333,110 @@ siginfo_t debugger::get_signal_info() {
     siginfo_t s_info;
     ptrace(PTRACE_GETSIGINFO, m_pid, nullptr, &s_info);
     return s_info;
+}
+
+void debugger::single_step_instruction() {
+    ptrace(PTRACE_SINGLESTEP, m_pid, nullptr, nullptr);
+    wait_for_signal();
+}
+
+void debugger::single_step_instruction_with_bp_check() {
+    if(addr_to_bp.count(get_program_counter())) {
+        step_over_breakpoint();
+    } else {
+        single_step_instruction();
+    }
+}
+
+// For stepping out, set breakpoint at return address and continue execution from there
+void debugger::step_out() {
+
+    auto frame_pointer = get_register_value_from_type(m_pid, register_type::rbp);
+    auto return_address = read_memory(frame_pointer + 8);
+
+    bool remove_bp = false;
+    if(addr_to_bp.count(return_address) == 0) {
+        addBreakpoint(return_address);
+        remove_bp = true;
+    }
+
+    continue_execution();
+
+    if (remove_bp) {
+        remove_breakpoint(return_address);
+    }
+
+}
+
+void debugger::remove_breakpoint(intptr_t addr) {
+    if(addr_to_bp.at(addr).is_enabled()) {
+        addr_to_bp.at(addr).disable();
+    }
+    addr_to_bp.erase(addr);
+}
+
+// For step_in, step over instruction till we reach the new line
+void debugger::step_in() {
+    auto line = get_line_entry_using_pc(get_offset_program_counter())->line;
+
+    while(get_line_entry_using_pc(get_offset_program_counter())->line == line) {
+        single_step_instruction_with_bp_check();
+    }
+
+    auto line_entry = get_line_entry_using_pc(get_offset_program_counter());
+    print_source(line_entry->file->path, line_entry->line, 2);
+}
+
+uint64_t debugger::get_offset_program_counter() {
+    return get_offset_load_address(get_program_counter());
+}
+
+uint64_t debugger::get_offset_dwarf_address(uint64_t addr) {
+    return addr + m_load_address;
+}
+
+// For step over, setting a breakpoint at every line in the current function
+void debugger::step_over() {
+
+    // Get function using program counter
+    auto function = get_func_using_pc(get_offset_program_counter());
+    auto func_start = dwarf::at_low_pc(function);
+    auto func_end = dwarf::at_high_pc(function);
+
+    // Get the first line of function
+    auto line = get_line_entry_using_pc(func_start);
+    auto start_line_entry = get_line_entry_using_pc(get_offset_program_counter());
+
+    // Vector to store the breakpoints we add for step_over (need to delete these breakpoints after step over is done)
+    vector<intptr_t> bp_to_delete;
+
+    // Iterating upto end of function and adding breakpoint which are not the start and there is no breakpoint beforehand
+    while(line->address < func_end) {
+        auto load_addr = get_offset_dwarf_address(line->address);
+        if((line->address != start_line_entry->address) && (addr_to_bp.count(load_addr) == 0)) {
+            addBreakpoint(load_addr);
+            bp_to_delete.push_back(load_addr);
+        }
+        line++;
+    }
+
+    // Similar to step_out, adding breakpoint to return address
+    auto frame_pointer = get_register_value_from_type(m_pid, register_type::rbp);
+    auto return_address = read_memory(frame_pointer + 8);
+
+    if(addr_to_bp.count(return_address) == 0) {
+        addBreakpoint(return_address);
+        bp_to_delete.push_back(return_address);
+    }
+
+    // Continuing the execution
+    continue_execution();
+
+    // Removing all breakpoints after step_over
+    for (auto bp: bp_to_delete) {
+        remove_breakpoint(bp);
+    }
+
 }
 
 void execute_debugee (const string& prog_name) {
